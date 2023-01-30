250ae8ba9bdc23795fe09f151109174e95aee551
Levy Campelo <42838123+levycampelo@users.noreply.github.com>
2023-01-30 14:43:21 -0300

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# SO Test Rocky Linux release 8.7 (IP 192.168.68.104) Master
+# SO Test Rocky Linux release 8.7 
+# IP 192.168.68.104 - Install only master
 # Install epel-release
 yum install epel-release -y
 # Install Ansible
